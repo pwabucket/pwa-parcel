263ffd6ee5b81421c0f145dcd5abaf7a0f64f53d
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-29 04:49:48 +0100

    feat: Added blockchain wallet form dialog

---
+++ src/components/WalletFormDialog.tsx
@@ -0,0 +1,28 @@
+import { Dialog } from "radix-ui";
+import type { Blockchain } from "../types";
+import { PopupDialog } from "./PopupDialog";
+
+interface WalletFormDialogProps extends Dialog.DialogProps {
+  blockchain: Blockchain;
+  children: React.ReactNode;
+}
+
+const WalletFormDialog = ({
+  children,
+  blockchain,
+  ...props
+}: WalletFormDialogProps) => {
+  return (
+    <PopupDialog
+      {...props}
+      open
+      title={blockchain.name || ""}
+      icon={blockchain.icon}
+      description="Enter the wallet details below"
+    >
+      {children}
+    </PopupDialog>
+  );
+};
+
+export { WalletFormDialog };
