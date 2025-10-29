12c8319a85395e8fdbb1ac56cd677c3b1edbbc64
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-29 07:47:53 +0100

    feat: Added Blockchain info component

---
+++ src/components/BlockchainInfo.tsx
@@ -0,0 +1,16 @@
+import { cn } from "../lib/utils";
+import type { Blockchain } from "../types";
+
+const BlockchainInfo = ({ blockchain }: { blockchain?: Blockchain }) => (
+  <h3
+    className={cn(
+      "flex flex-col gap-2 items-center justify-center",
+      "text-center text-purple-300 text-xl font-extralight"
+    )}
+  >
+    <img src={blockchain?.icon} alt="" className="size-16 shrink-0" />
+    {blockchain?.name}
+  </h3>
+);
+
+export { BlockchainInfo };
