e9b1907eeb85a00731eca3772563bf747e881916
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-29 04:51:21 +0100

    feat: Registered routes for home and split page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router";
+import { Home } from "./pages/Home";
+import { Split } from "./pages/Split";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="split" element={<Split />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
