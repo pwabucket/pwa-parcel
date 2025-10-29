37c6072aed7265b9afbea49580de04b1c79f95dc
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-29 17:07:30 +0100

    feat: Added Testnet address for USDT

--- src/blockchains/bsc/index.ts
+++ src/blockchains/bsc/index.ts
@@ -14,7 +14,9 @@ export const tokens: Token[] = [
     name: "Tether",
     symbol: "USDT",
     decimals: 18,
-    address: "0x55d398326f99059ff775485246999027b3197955",
+    address: import.meta.env.PROD
+      ? "0x55d398326f99059ff775485246999027b3197955"
+      : "0x337610d27c682e347c9cd60bd4b3b107c9d34ddd",
   },
 ];
 
