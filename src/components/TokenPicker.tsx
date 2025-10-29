e1aa62ea5039f3bf92e2676366575f446f640be1
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-29 07:52:33 +0100

    feat: Pass blockchain so as to display before list

--- src/components/TokenPicker.tsx
+++ src/components/TokenPicker.tsx
@@ -1,20 +1,24 @@
 import { tokenIcons } from "../resources/icons";
-import type { Token } from "../types";
+import type { Blockchain } from "../types";
 import { BasePicker } from "./BasePicker";
 import AppIcon from "../assets/icon.svg";
+import { BlockchainInfo } from "./BlockchainInfo";
 
 interface TokenPickerProps {
-  tokens: Token[];
+  blockchain: Blockchain;
   onSelect?: (tokenId: string) => void;
 }
 
-const TokenPicker = ({ tokens, onSelect }: TokenPickerProps) => {
+const TokenPicker = ({ blockchain, onSelect }: TokenPickerProps) => {
   return (
     <>
+      {/* Blockchain Info */}
+      <BlockchainInfo blockchain={blockchain} />
+
       <BasePicker
         title="Select a Token"
         items={[
-          ...tokens.map((token) => ({
+          ...blockchain.tokens.map((token) => ({
             id: token.id,
             name: `${token.name} (${token.symbol || ""})`,
             icon: tokenIcons[token.id],
