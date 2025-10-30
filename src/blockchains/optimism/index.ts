d9297a8f79d40b56c509ab126e5a6e19c9f4d81c
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-30 14:16:55 +0100

    feat: Added Polygon and Optimism Blockchain

---
+++ src/blockchains/optimism/index.ts
@@ -0,0 +1,38 @@
+import { EVMParcel, type EVMParcelOptions } from "../../parcels/EVMParcel";
+import type { Token } from "../../types";
+
+export const id = "optimism";
+export const name = "Optimism";
+export const tokens: Token[] = [
+  {
+    id: "eth",
+    name: "Ether",
+    symbol: "ETH",
+  },
+  {
+    id: "op",
+    name: "Optimism",
+    symbol: "OP",
+    decimals: 18,
+    address: "0x4200000000000000000000000000000000000042",
+  },
+  {
+    id: "usdt",
+    name: "Tether",
+    symbol: "USDT",
+    decimals: 18,
+    address: "0x94b008aa00579c1307b0ef2c499ad98a8ce58e58",
+  },
+];
+
+export class Parcel extends EVMParcel {
+  constructor(options: EVMParcelOptions = {}) {
+    super({
+      ...options,
+      network: "optimism",
+    });
+  }
+}
+
+export { EVMWalletForm as WalletForm } from "../../partials/evm/EVMWalletForm";
+export { EVMCustomTokenForm as CustomTokenForm } from "../../partials/evm/EVMCustomTokenForm";
---
+++ src/blockchains/polygon/index.ts
@@ -0,0 +1,31 @@
+import { EVMParcel, type EVMParcelOptions } from "../../parcels/EVMParcel";
+import type { Token } from "../../types";
+
+export const id = "polygon";
+export const name = "Polygon";
+export const tokens: Token[] = [
+  {
+    id: "pol",
+    name: "Polygon",
+    symbol: "POL",
+  },
+  {
+    id: "usdt",
+    name: "Tether",
+    symbol: "USDT",
+    decimals: 18,
+    address: "0xc2132d05d31c914a87c6611c10748aeb04b58e8f",
+  },
+];
+
+export class Parcel extends EVMParcel {
+  constructor(options: EVMParcelOptions = {}) {
+    super({
+      ...options,
+      network: "polygon",
+    });
+  }
+}
+
+export { EVMWalletForm as WalletForm } from "../../partials/evm/EVMWalletForm";
+export { EVMCustomTokenForm as CustomTokenForm } from "../../partials/evm/EVMCustomTokenForm";
