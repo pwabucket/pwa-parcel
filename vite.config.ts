1d851cc57c570a51fd8558db90d5bc9a30fbf63e
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-30 02:43:13 +0100

    feat: Used SWC Compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
-  ],
+  plugins: [tailwindcss(), react()],
 });
