0d3352279ce5ddcb0bdaaa5c655e932c653d4d42
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-30 02:45:39 +0100

    feat: Added Select Component

---
+++ src/components/Select.tsx
@@ -0,0 +1,25 @@
+import { cn } from "../lib/utils";
+
+const Select = (props: React.ComponentProps<"select">) => {
+  return (
+    <select
+      {...props}
+      className={cn(
+        "px-4 py-3 border border-neutral-900",
+        "focus:outline-none focus:ring-2 focus:ring-purple-500",
+        "disabled:opacity-50 disabled:cursor-not-allowed",
+        props.className
+      )}
+    />
+  );
+};
+
+const SelectOption = (props: React.ComponentProps<"option">) => {
+  return (
+    <option {...props} className={cn("bg-neutral-900", props.className)} />
+  );
+};
+
+Select.Option = SelectOption;
+
+export { Select };
