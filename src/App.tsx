f709cd860d0b46f766f90e0f625d06d9777e4b79
Sadiq Salau <sadiqsalau888@gmail.com>
2025-11-07 09:59:53 +0100

    feat: Acquire wakelock when mutating

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,16 @@ import { Route, Routes } from "react-router";
 import { Home } from "./pages/Home";
 import { Split } from "./pages/Split";
 import { Merge } from "./pages/Merge";
+import { useIsMutating } from "@tanstack/react-query";
+import { useWakeLock } from "./hooks/useWakeLock";
 
 function App() {
+  /* Track Ongoing Mutations */
+  const isMutating = useIsMutating();
+
+  /* Enable Wake Lock when Mutating */
+  useWakeLock(isMutating > 0);
+
   return (
     <>
       <Routes>
---
+++ src/hooks/useWakeLock.ts
@@ -0,0 +1,44 @@
+import { useEffect } from "react";
+import { useRef } from "react";
+
+const useWakeLock = (status = true) => {
+  const wakeLockRef = useRef<WakeLockSentinel | null>(null);
+
+  /** Acquire WakeLock */
+  useEffect(() => {
+    if (!status) return;
+
+    const requestWakeLock = async () => {
+      try {
+        wakeLockRef.current?.release();
+        wakeLockRef.current = await navigator.wakeLock.request("screen");
+      } catch (e) {
+        console.warn("WakeLock Error");
+        console.error(e);
+      }
+    };
+
+    const handleVisibilityChange = async () => {
+      if (
+        wakeLockRef.current !== null &&
+        document.visibilityState === "visible"
+      ) {
+        await requestWakeLock();
+      }
+    };
+
+    /** Watch Visibility Change */
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+
+    /** Request initial WakeLock */
+    requestWakeLock();
+
+    return () => {
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+      wakeLockRef.current?.release();
+      wakeLockRef.current = null;
+    };
+  }, [status]);
+};
+
+export { useWakeLock };
